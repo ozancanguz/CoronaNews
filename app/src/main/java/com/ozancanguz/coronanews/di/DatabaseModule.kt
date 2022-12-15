12940746733df367f5112d9535eba0e82d93f343
Ozan <ozancangu24@gmail.com>
2022-12-15 13:51:10 +0300

    -Database module created for di

---
+++ app/src/main/java/com/ozancanguz/coronanews/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.ozancanguz.coronanews.di
+
+import android.content.Context
+import androidx.room.Room
+import com.ozancanguz.coronanews.data.db.NewsDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        NewsDatabase::class.java,
+        "news_database"
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: NewsDatabase) = database.newsDao()
+
+}
