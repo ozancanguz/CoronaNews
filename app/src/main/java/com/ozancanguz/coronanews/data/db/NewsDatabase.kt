675d5dfa515986b5f9b18cb949e1cf9fc115b792
Ozan <ozancangu24@gmail.com>
2022-12-15 13:48:50 +0300

    -newsdatabase created

---
+++ app/src/main/java/com/ozancanguz/coronanews/data/db/NewsDatabase.kt
@@ -0,0 +1,19 @@
+package com.ozancanguz.coronanews.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+
+@Database(
+    entities = [NewsEntity::class],
+    version = 1,
+     exportSchema = false,
+    )
+
+@TypeConverters(TypeConverter::class)
+abstract class NewsDatabase:RoomDatabase() {
+
+    abstract fun newsDao(): NewsDao
+
+}
