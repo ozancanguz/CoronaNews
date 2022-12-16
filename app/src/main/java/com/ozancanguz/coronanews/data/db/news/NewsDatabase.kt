cbc50c5238cbade21db614055d5d04d02da52c09
Ozan <ozancangu24@gmail.com>
2022-12-16 11:39:52 +0300

    -news database modified

--- app/src/main/java/com/ozancanguz/coronanews/data/db/news/NewsDatabase.kt
+++ app/src/main/java/com/ozancanguz/coronanews/data/db/news/NewsDatabase.kt
@@ -3,10 +3,11 @@ package com.ozancanguz.coronanews.data.db.news
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.ozancanguz.coronanews.data.db.favorites.FavoritesEntity
 
 
 @Database(
-    entities = [NewsEntity::class],
+    entities = [NewsEntity::class,FavoritesEntity::class],
     version = 1,
      exportSchema = false,
     )
