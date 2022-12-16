09ef72c960f974970b614cd3ee3acf03f3880848
Ozan <ozancangu24@gmail.com>
2022-12-16 11:42:27 +0300

    -local data source modified

--- app/src/main/java/com/ozancanguz/coronanews/data/local/LocalDataSource.kt
+++ app/src/main/java/com/ozancanguz/coronanews/data/local/LocalDataSource.kt
@@ -1,5 +1,6 @@
 package com.ozancanguz.coronanews.data.local
 
+import com.ozancanguz.coronanews.data.db.favorites.FavoritesEntity
 import com.ozancanguz.coronanews.data.db.news.NewsDao
 import com.ozancanguz.coronanews.data.db.news.NewsEntity
 import kotlinx.coroutines.flow.Flow
@@ -18,6 +19,18 @@ class LocalDataSource@Inject constructor(private val newsDao: NewsDao){
     suspend fun insertNews(newsEntity: NewsEntity){
         return newsDao.insertNews(newsEntity)
     }
+
+
+    // get and list fav news
+    fun readFavoriteNews():Flow<List<FavoritesEntity>>{
+      return   newsDao.readFavoriteNews()
+    }
+
+    suspend fun insertFavorites(favoritesEntity: FavoritesEntity){
+        return newsDao.insertFavorites(favoritesEntity)
+    }
+
+
 
 
 }
