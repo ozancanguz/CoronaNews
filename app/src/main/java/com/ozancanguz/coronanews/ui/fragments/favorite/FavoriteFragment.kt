6aab473bec3e5772914ffc1b5508d20497e1e3a3
Ozan <ozancangu24@gmail.com>
2022-12-16 13:29:28 +0300

    -Favorites displayed in fav fragment

--- app/src/main/java/com/ozancanguz/coronanews/ui/fragments/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/ozancanguz/coronanews/ui/fragments/favorite/FavoriteFragment.kt
@@ -5,19 +5,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ozancanguz.coronanews.R
+import com.ozancanguz.coronanews.adapter.FavoritesAdapter
+import com.ozancanguz.coronanews.databinding.FragmentFavoriteBinding
+import com.ozancanguz.coronanews.viewmodels.CoronaNewsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class FavoriteFragment : Fragment() {
 
+    private var _binding: FragmentFavoriteBinding? = null
 
+    private val binding get() = _binding!!
+
+    private var favAdapter=FavoritesAdapter()
+    private val mainViewModel:CoronaNewsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        // set up rv
+        setUpRv()
+
+        // observe live data
+        observeLiveData()
+
+
+
+        return view
+    }
+
+    private fun setUpRv() {
+
+        binding.favoriteRecipesRecyclerView.layoutManager=LinearLayoutManager(requireContext())
+        binding.favoriteRecipesRecyclerView.adapter=favAdapter
+
+    }
+
+    private fun observeLiveData() {
+        mainViewModel.favoritesNews.observe(viewLifecycleOwner, Observer {
+             favAdapter.setFavData(it)
+        })
     }
 
 
